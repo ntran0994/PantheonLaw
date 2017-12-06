672ab1d2f261167bcd673e7c0e5656d9faf5417d
Hiep.Nguyen <hiep.nguyen@restaff.no>
2017-12-06 13:27:00 +0700

    add animation

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -10,6 +10,28 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.initOwlCarousel();
+  }
+
+  initOwlCarousel() {
+    $('.owl-clients-2').owlCarousel({
+      loop: true,
+      dots: true,
+      nav: false,
+      margin: 30,
+      responsive: {
+          0: {
+              items: 2
+          },
+          480: {
+              items: 3
+          },
+          768: {
+              items: 4
+          }
+      }
+  });
+
   }
 
 }
